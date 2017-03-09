a7b23331c006efadd2ef4c98a80bfb6496421698
Ryan Conrad <Ryan Conrad@129.21.66.102>
2017-03-08 22:54:29 -0500

    Add "Bad SQL query." popup on error

--- src/dsp/db/gui/frame/SelectDialogHandler.java
+++ src/dsp/db/gui/frame/SelectDialogHandler.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
 import dsp.db.gui.ComponentHandler;
 import dsp.db.gui.actions.CancelDialogAction;
 import dsp.db.query.DisorderlyQueryException;
@@ -61,6 +63,15 @@ public class SelectDialogHandler extends ComponentHandler {
 					rd.setVisible(true);
 					
 				} catch (SQLException | DisorderlyQueryException e) {
+					// Show an error message
+					JOptionPane.showMessageDialog(
+							null,
+							"Bad SQL query.",
+							"Error!",
+							JOptionPane.ERROR_MESSAGE);
+					
+					// TODO auditing
+					
 					e.printStackTrace();
 				}
 			}
